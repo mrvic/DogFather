6f8c4e9af0cf929e2cb6acfdbe0cb236dc0bbc04
mrvic <mrvic.predrag@gmail.com>
2017-05-21 01:48:12 +0200

    dodane migracije dog

--- database/migrations/2017_05_20_224822_create_dogs_table.php
+++ database/migrations/2017_05_20_224822_create_dogs_table.php
@@ -13,8 +13,21 @@ class CreateDogsTable extends Migration
      */
     public function up()
     {
+        // file:///C:/Users/SkyLucy/Code/Bone/docs/geobreeder.pdf
         Schema::create('dogs', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('microchip_no');
+            $table->integer('geobreeder_no');
+            $table->text('nickname');
+            $table->text('official_name');
+            $table->text('breed');
+            $table->date('dob');
+            $table->enum('gender', ['m','f']);
+            $table->text('color');
+            $table->text('hair');
+            $table->text('size');
+            $table->text('distinguishing_markings');
+            $table->enum('type', ['puppy','dog']);
             $table->timestamps();
         });
     }
